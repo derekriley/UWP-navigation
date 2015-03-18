6078f2ff46f186900e37b4b28c8497817475ca00
Able <ablerouse@gmail.com>
2015-03-18 16:55:51 -0500

    Added Zone class.

---
+++ app/src/main/java/uwp/cs/edu/parkingtracker/Zone.java
@@ -0,0 +1,48 @@
+package uwp.cs.edu.parkingtracker;
+
+import com.google.android.gms.maps.model.PolygonOptions;
+
+/**
+ * Created by Able on 3/18/2015.
+ */
+public class Zone {
+
+    private int fullness;
+    private String ID = new String();
+    private PolygonOptions poly = new PolygonOptions();
+
+    //constructor
+    public Zone (String ID, PolygonOptions poly) {
+
+        this.ID = ID;
+        this.poly = poly;
+
+    }
+
+    //setters
+    public void setFullness(int fullness) {
+        this.fullness = fullness;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setPoly(PolygonOptions poly) {
+        this.poly = poly;
+    }
+
+    //getters
+    public int getFullness(){
+        return fullness;
+    }
+
+    public String getID(){
+        return ID;
+    }
+
+    public PolygonOptions getPoly () {
+        return poly;
+    }
+
+}
