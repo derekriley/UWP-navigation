e359071f3bd1eaeba58413c8c4d8e6a3ccb504c2
Robin <rogoltermann@gmail.com>
2015-04-14 00:36:36 -0500

    reading and writing role preference

--- app/src/main/java/uwp/cs/edu/parkingtracker/MainActivity.java
+++ app/src/main/java/uwp/cs/edu/parkingtracker/MainActivity.java
@@ -2,23 +2,44 @@ package uwp.cs.edu.parkingtracker;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 
 
 public class MainActivity extends Activity {
+    public static final String PREFS_NAME = "UwpNavSettings";
     ProgressBar pBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // check if role preference is given, if not ask
+        SharedPreferences pref = getPreferences(0);
+
+        // TODO delete debug toasts
+        if(pref.getString("role","") == "") {
+            Toast.makeText(getApplicationContext(), "no pref given", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), pref.getString("role",""), Toast.LENGTH_LONG).show();
+        }
+
+        // overwriting role settings for testing purposes
+        // TODO: delete this
+        SharedPreferences.Editor prefEditor = pref.edit();
+        prefEditor.putString("role", "");
+        prefEditor.commit();
+
+
         getActionBar().hide();
 
         /* Google Analytics */
